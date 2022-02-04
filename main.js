742b7eef25693ae216370fe3ddb706c0fcf9b123
Abdulrahman <boodynaser@rocketmail.com>
2022-02-04 16:01:32 +0200

    Take user inputs

---
+++ main.js
@@ -0,0 +1,10 @@
+function Calculate() {
+    let firstInput = document.getElementById('firstvalue').value
+    let firstInputType=document.getElementById('firstvaluetype').value
+
+    let secondInput = document.getElementById('secondvalue').value
+    let secondInputType = document.getElementById('secondvaluetype').value
+    
+    console.log(firstInputType);
+    console.log(secondInput);
+}
