54db874bca1964cc14077b1c0639ba7334ee14c3
Abdulrahman <boodynaser@rocketmail.com>
2022-02-04 17:29:45 +0200

    Update availabe options for calculation

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+let IntialCalculatedOptions = ["Radius","Angle","Chord","SurfaceArea","ArcLength"];
 function Calculate() {
     let firstInput = document.getElementById('firstvalue').value
     let firstInputType=document.getElementById('firstvaluetype').value
 
     let secondInput = document.getElementById('secondvalue').value
     let secondInputType = document.getElementById('secondvaluetype').value
-    
-    console.log(firstInputType);
-    console.log(secondInput);
+
+
+    let UpdatedCalculatedOptions = IntialCalculatedOptions.filter((element)=>{ 
+        return (element !==firstInputType)
+    });
+    UpdatedCalculatedOptions = UpdatedCalculatedOptions.filter((element)=>{ 
+        return (element !==secondInputType)
+    });
+
+    document.getElementById('optionone').innerText=UpdatedCalculatedOptions.pop();
+    document.getElementById('optiontwo').innerText=UpdatedCalculatedOptions.pop();
+    document.getElementById('optionthree').innerText=UpdatedCalculatedOptions.pop();
 }
